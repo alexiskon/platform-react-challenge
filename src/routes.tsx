0f0c9471e9324ec8997a460462c0d1d77d050efa
Alexandros Kontos <alexiskon01@gmail.com>
2025-02-02 18:23:13 +0200

    github pages deploy

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,14 +1,14 @@
 import React from "react";
 import Favourites from "./pages/Favourites/Favourites";
-import { useRoutes } from "react-router-dom";
+import { Navigate, useRoutes } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import Breeds from "./pages/Breeds/Breeds";
 
 export const routeConfig = () => [
+    { path: "*", element: <Navigate to="/" replace /> },
     {path: '/', element: <Home />, label: 'Home', headerDisplay: true},
-    {path: ":id?", element: <Home />, label: '', headerDisplay: false},
+    {path: "/:id?", element: <Home />, label: '', headerDisplay: false},
     {path: '/breeds', element: <Breeds />, label: 'Breeds', headerDisplay: true},
-    {path: '/breeds/:id?', element: <Breeds />, label: '', headerDisplay: false},
     {path: '/favourites', element: <Favourites />, label: 'Favourites', headerDisplay: true}
 ]
 
--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import path from 'path';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/platform-react-challenge/',
   resolve: {
     alias: {
       '@images': path.resolve(__dirname, './src/assets/images'),
